e9c609466d13d6e24c9c9afa17964f89ba285b63
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-24 15:46:19 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
 var chatMsg = document.getElementById("chatMsg");
+var userMsg = document.getElementById("userMsg");
 
 
 function sendMessage(event){
-    if(event.keycode === 13){
-        var elem = event.target;
-        
+    if(event.keyCode === 13){
+    var elem = event.target;
+    var message = elem.value.trim()
+      userMsg.innerHTML += `<div class = "my-chat">  ${message}</div> `
+      event.target.value = ""  
     }
 }

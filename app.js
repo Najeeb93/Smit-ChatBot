8aff842c62d82b66b5449650bc20c7baa58df8c9
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-05-24 16:46:29 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ var userMsg = document.getElementById("userMsg");
 function sendMessage(event){
     if(event.keyCode === 13){
     var elem = event.target;
-    var message = elem.value.trim()
+     var message = elem.value.trim();
+
       userMsg.innerHTML += `<div class = "my-chat">  ${message}</div> `
+      if(['hi', 'hey', 'hello', 'hy',  'salam', 'assalam-0-alikum', 'assalam o alikum'].indexOf(elem.value) !== -1){
+        setTimeout(function() {
+            chatMsg.innerHTML += '<div class ="client-chat" > Hello </div>' 
+        }, 2000);
+      }
+    
+      
       event.target.value = ""  
     }
 }
